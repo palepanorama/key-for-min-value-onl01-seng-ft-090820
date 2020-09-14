e84b4adbb6b1dd9d710f33084871079c2f3b5543
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-14 13:24:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  low_key = name_hash.reduce do |key, value|
+  low_key == name_hash.reduce do |key, value|
     key.last > value.last ? value : key 
   end 
   if name_hash == {}
