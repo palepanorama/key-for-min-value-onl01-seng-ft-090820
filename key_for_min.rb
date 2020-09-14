891149204dbf5c0f78c174b7ae30a8b32ecea473
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-14 13:13:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 def key_for_min_value(name_hash)
   low_value = 0 
   low_key = nil 
-  if low_value == 0 
-    low_value = value 
-    low_key = key 
+  name_hash.each do |key, value|
+    if low_key = 0 
+      low_value = value 
+      low_key = key 
+    end 
   end 
-end 
   low_key 
 end 
 
